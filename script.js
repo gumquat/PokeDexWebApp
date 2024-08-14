0edbdaed7997988ce50df72aacd9a50765288633
gumquat <gumquat@gmail.com>
2024-08-14 11:20:55 -0500

    current error: pokedex will not load more than 537 pokemon for some reason. Check the API for reasons why

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Total number of Pokémon to be loaded
 const pokemonCount = 1008;
 // Number of Pokémon to load per batch
-const pokemonPerLoad = 537;
+const pokemonPerLoad = 500;
 // Object to store all loaded Pokémon data
 var pokedex = {};
 // Keep track of how many Pokémon have been loaded
@@ -17,62 +17,62 @@ window.onload = async function()
     console.log(pokedex);
 }
 
+
 // Function to load more Pokémon
 async function loadMorePokemon()
 {
-    // Calculate the range of Pokémon to load
     let start = currentLoadedCount + 1;
-    let end = Math.min(start + pokemonPerLoad - 1, pokemonCount);
-    
+    let remainingPokemon = pokemonCount - currentLoadedCount;
+    let pokemonToLoad = Math.min(remainingPokemon, pokemonPerLoad);
+    let end = start + pokemonToLoad - 1;
+
     // Add loading indicator
     let loadingIndicator = document.createElement("p");
     loadingIndicator.id = "loading-indicator";
     loadingIndicator.innerText = "Loading...";
     document.getElementById("pokemon-list").appendChild(loadingIndicator);
     
-    // Fetch the Pokémon data
     await getPokemonBulk(start, end);
     
     // Remove loading indicator
-    document.getElementById("loading-indicator").remove();
+    loadingIndicator.remove();
     
-    // Create and append DOM elements for each loaded Pokémon
-    for (let i = start; i <= end; i++) {
-        let pokemon = document.createElement("div");
-        pokemon.id = i;
-        pokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
-        pokemon.classList.add("pokemon-name");
-        pokemon.addEventListener("click", updatePokemon);
-        document.getElementById("pokemon-list").append(pokemon);
-    }
-    
-    // If this is the first load, set the initial Pokémon description
-    if (currentLoadedCount === 0) {
-        document.getElementById("pokemon-description").innerText = pokedex[1]["desc"];
-        updatePokemon.call(document.getElementById("1"));
-    }
-    
-    // Update the count of loaded Pokémon
-    currentLoadedCount = end;
-    
-    // Manage the "Load More" button
-    let loadMoreButton = document.getElementById("load-more-button");
-    if (currentLoadedCount < pokemonCount) {
-        if (!loadMoreButton) {
-            loadMoreButton = document.createElement("button");
-            loadMoreButton.id = "load-more-button";
-            loadMoreButton.innerText = "Load More";
-            loadMoreButton.addEventListener("click", loadMorePokemon);
+    // Check if new Pokémon were actually loaded
+    if (currentLoadedCount >= start) {
+        for (let i = start; i <= currentLoadedCount; i++) {
+            let pokemon = document.createElement("div");
+            pokemon.id = i;
+            pokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
+            pokemon.classList.add("pokemon-name");
+            pokemon.addEventListener("click", updatePokemon);
+            document.getElementById("pokemon-list").append(pokemon);
         }
-        document.getElementById("pokemon-list").appendChild(loadMoreButton);
-    } else if (loadMoreButton) {
-        loadMoreButton.remove();
-    }
+        
+        if (start === 1) {
+            document.getElementById("pokemon-description").innerText = pokedex[1]["desc"];
+            updatePokemon.call(document.getElementById("1"));
+        }
+        
+        // Manage the "Load More" button
+        let loadMoreButton = document.getElementById("load-more-button");
+        if (currentLoadedCount < pokemonCount) {
+            if (!loadMoreButton) {
+                loadMoreButton = document.createElement("button");
+                loadMoreButton.id = "load-more-button";
+                loadMoreButton.innerText = "Load More";
+                loadMoreButton.addEventListener("click", loadMorePokemon);
+            }
+            document.getElementById("pokemon-list").appendChild(loadMoreButton);
+        } else if (loadMoreButton) {
+            loadMoreButton.remove();
+        }
 
-    // Scroll to show the newly added Pokémon
-    let pokemonList = document.getElementById("pokemon-list");
-    pokemonList.scrollTop = pokemonList.scrollHeight - pokemonList.clientHeight;
+        // Scroll to show the newly added Pokémon
+        let pokemonList = document.getElementById("pokemon-list");
+        pokemonList.scrollTop = pokemonList.scrollHeight - pokemonList.clientHeight;
+    }
 }
+
 
 async function getPokemonBulk(start, end)
 {
@@ -125,6 +125,9 @@ async function getPokemonBulk(start, end)
                 "desc": pokemonDesc
             };
         }
+        
+        // Update currentLoadedCount
+        currentLoadedCount = end;
     } catch (error) {
         console.error("Failed to fetch Pokémon data:", error);
         alert("Failed to load Pokémon data. Please try again later.");
@@ -135,10 +138,10 @@ async function getPokemonBulk(start, end)
             loadingIndicator.remove();
         }
         
-        // Revert the currentLoadedCount
-        currentLoadedCount = start - 1;
+        // We don't revert currentLoadedCount here, as it wasn't updated in case of an error
     }
 }
+
 
 // Function to update the displayed Pokémon information
 function updatePokemon() {
